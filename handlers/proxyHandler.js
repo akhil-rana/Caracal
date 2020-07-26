643864498c62dd430044d0f4853d8e45dbab3e03
Akhil Rana <akhilrana0001@gmail.com>
2020-07-27 01:24:37 +0530

    Revert "change n?"
    This reverts commit b420e223ed38c4828eabd863231adc673269001b.

--- handlers/proxyHandler.js
+++ handlers/proxyHandler.js
@@ -1,7 +1,6 @@
 var proxy = require('http-proxy-middleware');
 
-proxyHandler = function(target, n){
-  n = n || 2
+proxyHandler = function(PATH){
   return function(req, res, next) {
     proxy({
       secure: false,
@@ -11,13 +10,13 @@ proxyHandler = function(target, n){
         next(err);
       },
       changeOrigin: true,
-      target: target,
+      target: PATH,
       pathRewrite: function(path, req) {
-        console.log(target)
+        console.log(PATH)
         console.log(path)
         // NOTE -- this may need to change if the original url has more subdirs or so added
         var splitPath = path.split('/');
-        return '/' + splitPath.slice(n, splitPath.length).join('/');
+        return '/' + splitPath.slice(2, splitPath.length).join('/');
       },
       onProxyReq: function(proxyReq, req, res) {
         if (req.method == 'POST') {
